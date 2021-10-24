d064e61e1442cbdd8b61308ec49f541743e7d22e
Mikita Navicki <bulbanik91@gmail.com>
2021-10-24 21:42:50 +0300

    Создание и хранение HTML-шаблонов на уровне проекта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
-    return HttpResponse('Список публикаций')
+    template = 'posts/group_list.html'
+    return render(request, template)
