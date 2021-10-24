0d84a6c7f7d27822d6526264bfe52af6c9ac49ab
Mikita Navicki <bulbanik91@gmail.com>
2021-10-24 23:33:25 +0300

    Рендеринг и вывод переменных в HTML-шаблон

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Yatube'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Yatube'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
