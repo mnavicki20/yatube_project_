fbfd1008ed1aeadc5284ca8e7cea44bd2a35af35
Mikita Navicki <bulbanik91@gmail.com>
2021-10-25 11:58:34 +0300

    Создание класса PostAdmin и его регистрация

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
+admin.site.register(Post, PostAdmin)
