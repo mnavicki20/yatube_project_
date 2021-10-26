ef61ee4e8cf507f3cd17e94ff90732e0bbbe9c77
Mikita Navicki <bulbanik91@gmail.com>
2021-10-27 00:02:21 +0300

    Миграция обновлённых моделей

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -22,8 +22,8 @@ class Post(models.Model):
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
