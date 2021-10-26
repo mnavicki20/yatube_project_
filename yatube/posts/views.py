29585e837bd0838e3650efbe57554ef76544a1a9
Mikita Navicki <bulbanik91@gmail.com>
2021-10-26 20:10:36 +0300

    Скорректирована ссылка в строке "все записи группы"

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
     context = {
         'group': group,
